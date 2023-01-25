4d60f2ba650ffa6d3864ab5701cdf685495f0f56
Bnyro <bnyro@tutanota.com>
2023-01-25 10:44:06 +0100

    FAB to create new contact

--- app/src/main/java/com/bnyro/contacts/ui/components/ContactsPage.kt
+++ app/src/main/java/com/bnyro/contacts/ui/components/ContactsPage.kt
@@ -6,7 +6,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Create
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -40,6 +44,14 @@ fun ContactsPage(contacts: List<ContactData>?) {
                         ContactItem(it)
                     }
                 }
+            }
+            FloatingActionButton(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(Alignment.BottomEnd),
+                onClick = { /*TODO*/ }
+            ) {
+                Icon(Icons.Default.Create, null)
             }
         } else {
             CircularProgressIndicator(
