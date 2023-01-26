38971f3458d90ea85392620408c4ed193ef5949a
Bnyro <bnyro@tutanota.com>
2023-01-26 17:57:10 +0100

    Swipe to dismiss for contact deletion

--- app/src/main/java/com/bnyro/contacts/ui/components/ContactItem.kt
+++ app/src/main/java/com/bnyro/contacts/ui/components/ContactItem.kt
@@ -11,9 +11,13 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.DismissDirection
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SwipeToDismiss
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDismissState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -23,65 +27,102 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.bnyro.contacts.R
 import com.bnyro.contacts.enums.SortOrder
 import com.bnyro.contacts.obj.ContactData
+import com.bnyro.contacts.ui.components.dialogs.ConfirmationDialog
 import com.bnyro.contacts.ui.models.ContactsModel
 import com.bnyro.contacts.ui.screens.SingleContactScreen
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ContactItem(contact: ContactData, sortOrder: SortOrder) {
     val shape = RoundedCornerShape(20.dp)
-    var showContactScreen by remember {
-        mutableStateOf(false)
-    }
     val viewModel: ContactsModel = viewModel()
     val context = LocalContext.current
 
-    ElevatedCard(
-        modifier = Modifier.padding(horizontal = 10.dp, vertical = 5.dp)
-            .fillMaxWidth()
-            .clip(shape)
-            .clickable {
-                showContactScreen = true
-            },
-        shape = shape
-    ) {
-        Row(
-            modifier = Modifier
-                .padding(12.dp)
-                .fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Box(
-                modifier = Modifier
-                    .size(40.dp)
-                    .background(shape = CircleShape, color = MaterialTheme.colorScheme.primary)
-            ) {
-                Text(
-                    modifier = Modifier.align(Alignment.Center),
-                    text = (contact.displayName?.firstOrNull() ?: "").toString(),
-                    color = MaterialTheme.colorScheme.onPrimary
-                )
-            }
-            Spacer(modifier = Modifier.width(20.dp))
-            Text(
-                when (sortOrder) {
-                    SortOrder.FIRSTNAME -> "${contact.firstName ?: ""} ${contact.surName ?: ""}".trim()
-                    SortOrder.SURNAME -> if (contact.surName != null) {
-                        "${contact.surName}, ${contact.firstName}"
-                    } else {
-                        contact.displayName.orEmpty()
-                    }
-                }
-            )
-        }
+    var showContactScreen by remember {
+        mutableStateOf(false)
     }
+    var showDelete by remember {
+        mutableStateOf(false)
+    }
+
+    val state = rememberDismissState(
+        confirmValueChange = {
+            showDelete = true
+            false
+        }
+    )
+
+    SwipeToDismiss(
+        state = state,
+        background = {},
+        directions = setOf(DismissDirection.StartToEnd),
+        dismissContent = {
+            ElevatedCard(
+                modifier = Modifier.padding(horizontal = 10.dp, vertical = 5.dp)
+                    .fillMaxWidth()
+                    .clip(shape)
+                    .clickable {
+                        showContactScreen = true
+                    },
+                shape = shape
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(12.dp)
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .size(40.dp)
+                            .background(
+                                shape = CircleShape,
+                                color = MaterialTheme.colorScheme.primary
+                            )
+                    ) {
+                        Text(
+                            modifier = Modifier.align(Alignment.Center),
+                            text = (contact.displayName?.firstOrNull() ?: "").toString(),
+                            color = MaterialTheme.colorScheme.onPrimary
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(20.dp))
+                    Text(
+                        when (sortOrder) {
+                            SortOrder.FIRSTNAME -> "${contact.firstName ?: ""} ${contact.surName ?: ""}".trim()
+                            SortOrder.SURNAME -> if (contact.surName != null) {
+                                "${contact.surName}, ${contact.firstName}"
+                            } else {
+                                contact.displayName.orEmpty()
+                            }
+                        }
+                    )
+                }
+            }
+        }
+    )
 
     if (showContactScreen) {
         SingleContactScreen(viewModel.loadAdvancedContactData(context, contact)) {
             showContactScreen = false
+        }
+    }
+
+    if (showDelete) {
+        ConfirmationDialog(
+            onDismissRequest = {
+                showDelete = false
+            },
+            title = stringResource(R.string.delete_contact),
+            text = stringResource(R.string.irreversible)
+        ) {
+            viewModel.deleteContact(context, contact)
         }
     }
 }
