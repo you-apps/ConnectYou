238ebd5c9c2635c4feeec0f3e00c76fd752d775c
Bnyro <bnyro@tutanota.com>
2023-03-01 09:45:58 +0100

    Allow searching contacts when adding to existing

--- app/src/main/java/com/bnyro/contacts/ui/components/dialogs/AddToContactDialog.kt
+++ app/src/main/java/com/bnyro/contacts/ui/components/dialogs/AddToContactDialog.kt
@@ -1,6 +1,7 @@
 package com.bnyro.contacts.ui.components.dialogs
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -8,6 +9,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -28,6 +31,7 @@ import com.bnyro.contacts.ui.models.ContactsModel
 import com.bnyro.contacts.ui.screens.EditorScreen
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddToContactDialog(
     newNumber: String
@@ -46,6 +50,10 @@ fun AddToContactDialog(
     val scope = rememberCoroutineScope()
 
     if (showDialog) {
+        var searchQuery by remember {
+            mutableStateOf("")
+        }
+
         AlertDialog(
             onDismissRequest = { showDialog = false },
             confirmButton = {
@@ -57,27 +65,47 @@ fun AddToContactDialog(
                 Text(text = stringResource(R.string.add_to_contact))
             },
             text = {
-                LazyColumn(
-                    modifier = Modifier.height(400.dp)
-                ) {
-                    items(contactsModel.contacts.orEmpty()) {
-                        Text(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .clip(RoundedCornerShape(20.dp))
-                                .clickable {
-                                    scope.launch {
-                                        contactToEdit = contactsModel.loadAdvancedContactData(it).apply {
-                                            it.numbers = it.numbers.toMutableList().apply {
-                                                add(ValueWithType(newNumber, 0))
+                Column {
+                    OutlinedTextField(
+                        modifier = Modifier.padding(bottom = 10.dp),
+                        value = searchQuery,
+                        onValueChange = { searchQuery = it },
+                        label = {
+                            Text(stringResource(R.string.search))
+                        }
+                    )
+
+                    LazyColumn(
+                        modifier = Modifier.height(400.dp)
+                    ) {
+                        items(
+                            contactsModel.contacts.orEmpty()
+                                .filter {
+                                    it.displayName.orEmpty().lowercase().contains(
+                                        searchQuery.lowercase()
+                                    )
+                                }
+                        ) {
+                            Text(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .clip(RoundedCornerShape(20.dp))
+                                    .clickable {
+                                        scope.launch {
+                                            contactToEdit = contactsModel.loadAdvancedContactData(
+                                                it
+                                            ).apply {
+                                                it.numbers = it.numbers.toMutableList().apply {
+                                                    add(ValueWithType(newNumber, 0))
+                                                }
                                             }
                                         }
+                                        showDialog = false
                                     }
-                                    showDialog = false
-                                }
-                                .padding(vertical = 15.dp, horizontal = 20.dp),
-                            text = it.displayName.orEmpty()
-                        )
+                                    .padding(vertical = 15.dp, horizontal = 20.dp),
+                                text = it.displayName.orEmpty()
+                            )
+                        }
                     }
                 }
             }
