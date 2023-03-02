21e8a91e37902c754771cc15090fea65c0657736
Bnyro <bnyro@tutanota.com>
2023-03-02 16:53:50 +0100

    Fix ktlint

--- app/src/main/java/com/bnyro/contacts/ui/components/editor/DatePickerEditor.kt
+++ app/src/main/java/com/bnyro/contacts/ui/components/editor/DatePickerEditor.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
