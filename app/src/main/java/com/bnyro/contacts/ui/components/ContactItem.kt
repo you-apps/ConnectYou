b313fd164bfc728e0074978f1fed2d8a85259a81
Bnyro <bnyro@tutanota.com>
2023-03-02 16:03:01 +0100

    Remove swipe to delete gesture (closes #80)

--- app/src/main/java/com/bnyro/contacts/ui/components/ContactItem.kt
+++ app/src/main/java/com/bnyro/contacts/ui/components/ContactItem.kt
@@ -16,15 +16,11 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
-import androidx.compose.material3.DismissDirection
-import androidx.compose.material3.DismissValue
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.SwipeToDismiss
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberDismissState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -35,15 +31,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.bnyro.contacts.R
 import com.bnyro.contacts.enums.SortOrder
 import com.bnyro.contacts.ext.notAName
 import com.bnyro.contacts.obj.ContactData
-import com.bnyro.contacts.ui.components.dialogs.ConfirmationDialog
 import com.bnyro.contacts.ui.models.ContactsModel
 import com.bnyro.contacts.ui.screens.SingleContactScreen
 import kotlinx.coroutines.runBlocking
@@ -59,98 +51,75 @@ fun ContactItem(
 ) {
     val shape = RoundedCornerShape(20.dp)
     val viewModel: ContactsModel = viewModel()
-    val context = LocalContext.current
 
     var showContactScreen by remember {
         mutableStateOf(false)
     }
-    var showDelete by remember {
-        mutableStateOf(false)
-    }
 
-    val dismissState = rememberDismissState(
-        confirmValueChange = {
-            when (it) {
-                DismissValue.DismissedToEnd -> {
-                    showDelete = true
+    ElevatedCard(
+        modifier = Modifier
+            .padding(horizontal = 10.dp, vertical = 5.dp)
+            .fillMaxWidth()
+            .clip(shape)
+            .combinedClickable(
+                onClick = {
+                    if (!onSinglePress()) showContactScreen = true
+                },
+                onLongClick = {
+                    onLongPress.invoke()
                 }
-                else -> {}
-            }
-            false
-        }
-    )
-
-    SwipeToDismiss(
-        state = dismissState,
-        directions = setOf(DismissDirection.StartToEnd),
-        background = {},
-        dismissContent = {
-            ElevatedCard(
+            ),
+        shape = shape
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(12.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
                 modifier = Modifier
-                    .padding(horizontal = 10.dp, vertical = 5.dp)
-                    .fillMaxWidth()
-                    .clip(shape)
-                    .combinedClickable(
-                        onClick = {
-                            if (!onSinglePress()) showContactScreen = true
-                        },
-                        onLongClick = {
-                            onLongPress.invoke()
-                        }
-                    ),
-                shape = shape
+                    .size(40.dp)
+                    .background(
+                        shape = CircleShape,
+                        color = MaterialTheme.colorScheme.primary
+                    )
             ) {
-                Row(
-                    modifier = Modifier
-                        .padding(12.dp)
-                        .fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Box(
-                        modifier = Modifier
-                            .size(40.dp)
-                            .background(
-                                shape = CircleShape,
-                                color = MaterialTheme.colorScheme.primary
-                            )
-                    ) {
-                        val thumbnail = contact.thumbnail ?: contact.photo
-                        if (selected) {
-                            Icon(
-                                modifier = Modifier.align(Alignment.Center),
-                                imageVector = Icons.Default.Check,
-                                contentDescription = null,
-                                tint = MaterialTheme.colorScheme.background
-                            )
-                        } else if (thumbnail == null) {
-                            Text(
-                                modifier = Modifier.align(Alignment.Center),
-                                text = (contact.displayName?.firstOrNull() ?: "").toString(),
-                                color = MaterialTheme.colorScheme.onPrimary
-                            )
-                        } else {
-                            Image(
-                                modifier = Modifier
-                                    .fillMaxSize()
-                                    .clip(CircleShape),
-                                bitmap = thumbnail.asImageBitmap(),
-                                contentDescription = null,
-                                contentScale = ContentScale.Crop
-                            )
-                        }
-                    }
-                    Spacer(modifier = Modifier.width(20.dp))
+                val thumbnail = contact.thumbnail ?: contact.photo
+                if (selected) {
+                    Icon(
+                        modifier = Modifier.align(Alignment.Center),
+                        imageVector = Icons.Default.Check,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.background
+                    )
+                } else if (thumbnail == null) {
                     Text(
-                        when {
-                            sortOrder == SortOrder.FIRSTNAME -> "${contact.firstName ?: ""} ${contact.surName ?: ""}"
-                            sortOrder == SortOrder.LASTNAME && !contact.surName.notAName() && !contact.firstName.notAName() -> "${contact.surName}, ${contact.firstName}"
-                            else -> contact.displayName.orEmpty()
-                        }.trim()
+                        modifier = Modifier.align(Alignment.Center),
+                        text = (contact.displayName?.firstOrNull() ?: "").toString(),
+                        color = MaterialTheme.colorScheme.onPrimary
+                    )
+                } else {
+                    Image(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .clip(CircleShape),
+                        bitmap = thumbnail.asImageBitmap(),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop
                     )
                 }
             }
+            Spacer(modifier = Modifier.width(20.dp))
+            Text(
+                when {
+                    sortOrder == SortOrder.FIRSTNAME -> "${contact.firstName ?: ""} ${contact.surName ?: ""}"
+                    sortOrder == SortOrder.LASTNAME && !contact.surName.notAName() && !contact.firstName.notAName() -> "${contact.surName}, ${contact.firstName}"
+                    else -> contact.displayName.orEmpty()
+                }.trim()
+            )
         }
-    )
+    }
 
     if (showContactScreen) {
         val data = runBlocking {
@@ -158,18 +127,6 @@ fun ContactItem(
         }
         SingleContactScreen(data) {
             showContactScreen = false
-        }
-    }
-
-    if (showDelete) {
-        ConfirmationDialog(
-            onDismissRequest = {
-                showDelete = false
-            },
-            title = stringResource(R.string.delete_contact),
-            text = stringResource(R.string.irreversible)
-        ) {
-            viewModel.deleteContacts(listOf(contact))
         }
     }
 }
