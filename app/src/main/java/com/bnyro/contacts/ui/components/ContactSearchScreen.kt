d1eae4d23fb271db7c0257e7f72b8a48b84558c7
Bnyro <bnyro@tutanota.com>
2023-09-07 22:19:12 +0200

    fix UI freezes when searching large lists, more precise search

--- app/src/main/java/com/bnyro/contacts/ui/components/ContactSearchScreen.kt
+++ app/src/main/java/com/bnyro/contacts/ui/components/ContactSearchScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
@@ -29,7 +30,10 @@ import com.bnyro.contacts.obj.ContactData
 import com.bnyro.contacts.obj.FilterOptions
 import com.bnyro.contacts.ui.components.base.ClickableIcon
 import com.bnyro.contacts.ui.components.base.FullScreenDialog
+import com.bnyro.contacts.ui.models.ContactsModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 
 @Composable
 fun ContactSearchScreen(
@@ -41,17 +45,37 @@ fun ContactSearchScreen(
         val focusRequester = remember {
             FocusRequester()
         }
+
         LaunchedEffect(Unit) {
             delay(100)
             focusRequester.requestFocus()
         }
+
         var searchQuery by remember {
             mutableStateOf("")
+        }
+        var visibleContacts by remember {
+            mutableStateOf(contacts)
+        }
+        LaunchedEffect(searchQuery) {
+            withContext(Dispatchers.IO) {
+                val query = searchQuery.lowercase()
+                visibleContacts = contacts.filter {
+                    val contactInfoStrings = listOf(it.numbers, it.emails, it.addresses, it.notes, it.websites, it.events)
+                        .flatten()
+                        .map { (value, _) -> value } + listOf(it.organization, it.nickName, it.displayName)
+
+                    contactInfoStrings.filterNotNull().any { str ->
+                        str.contains(query)
+                    }
+                }
+            }
         }
 
         Column(Modifier.fillMaxSize()) {
             TextField(
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .fillMaxWidth()
                     .padding(horizontal = 8.dp)
                     .padding(top = 8.dp)
                     .focusRequester(focusRequester),
@@ -74,12 +98,8 @@ fun ContactSearchScreen(
                     unfocusedIndicatorColor = Color.Transparent
                 )
             )
-            val query = searchQuery.lowercase()
             ContactsList(
-                contacts = contacts.filter {
-                    it.displayName.orEmpty().lowercase().contains(query) ||
-                        it.numbers.any { number -> number.value.contains(query) }
-                },
+                contacts = visibleContacts,
                 filterOptions = filterOptions,
                 scrollConnection = null,
                 selectedContacts = emptyList<ContactData>().toMutableList()
