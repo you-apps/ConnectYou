169ec43cbd429c1e07360a48b3d514b43a0f3152
Bnyro <bnyro@tutanota.com>
2024-04-01 22:35:02 +0200

    fix: support secondary and teritary fields when importing contact (addresses #383)

--- app/src/main/java/com/bnyro/contacts/ui/activities/MainActivity.kt
+++ app/src/main/java/com/bnyro/contacts/ui/activities/MainActivity.kt
@@ -16,6 +16,7 @@ import com.bnyro.contacts.ui.screens.MainAppContent
 import com.bnyro.contacts.ui.theme.ConnectYouTheme
 import com.bnyro.contacts.util.BackupHelper
 import com.bnyro.contacts.util.ContactsHelper
+import com.bnyro.contacts.util.IntentHelper
 import java.net.URLDecoder
 
 class MainActivity : BaseActivity() {
@@ -49,34 +50,7 @@ class MainActivity : BaseActivity() {
     private fun getInsertContactData(): ContactData? {
         return when {
             intent?.action == Intent.ACTION_INSERT -> {
-                val name = intent.getStringExtra(Intents.Insert.NAME)
-                    ?: intent.getStringExtra(Intents.Insert.PHONETIC_NAME)
-                ContactData(
-                    displayName = name,
-                    firstName = name?.split(" ")?.firstOrNull(),
-                    surName = name?.split(" ", limit = 2)?.lastOrNull(),
-                    organization = intent.getStringExtra(Intents.Insert.COMPANY),
-                    numbers = listOfNotNull(
-                        intent.getStringExtra(Intents.Insert.PHONE)?.let {
-                            ValueWithType(it, 0)
-                        }
-                    ),
-                    emails = listOfNotNull(
-                        intent.getStringExtra(Intents.Insert.EMAIL)?.let {
-                            ValueWithType(it, 0)
-                        }
-                    ),
-                    notes = listOfNotNull(
-                        intent.getStringExtra(Intents.Insert.NOTES)?.let {
-                            ValueWithType(it, 0)
-                        }
-                    ),
-                    addresses = listOfNotNull(
-                        intent.getStringExtra(Intents.Insert.POSTAL)?.let {
-                            ValueWithType(it, 0)
-                        }
-                    )
-                )
+                IntentHelper.extractContactFromIntent(intent)
             }
 
             intent?.getStringExtra("action") == "create" -> ContactData()
