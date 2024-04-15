a39ca3a4e3195bf017a8893ebd5b289b6f38839f
Bnyro <bnyro@tutanota.com>
2024-04-15 12:33:06 +0200

    fix: automatic backup options overflow (closes #377)

--- app/src/main/java/com/bnyro/contacts/ui/components/prefs/BlockPreference.kt
+++ app/src/main/java/com/bnyro/contacts/ui/components/prefs/BlockPreference.kt
@@ -1,11 +1,12 @@
 package com.bnyro.contacts.ui.components.prefs
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
@@ -25,58 +26,57 @@ fun BlockPreference(
     entries: List<String>,
     onSelectionChange: (Int) -> Unit = {}
 ) {
-    Column(
+    val state = rememberScrollState()
+
+    Row(
         modifier = Modifier
+            .fillMaxWidth()
             .padding(vertical = 8.dp)
+            .horizontalScroll(state)
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-        ) {
-            val cornerRadius = 20.dp
+        val cornerRadius = 20.dp
 
-            var selectedItem by rememberPreference(key = preferenceKey, defaultValue = 0)
+        var selectedItem by rememberPreference(key = preferenceKey, defaultValue = 0)
 
-            entries.forEachIndexed { index, entry ->
-                val startRadius = if (index != 0) 0.dp else cornerRadius
-                val endRadius = if (index == entries.size - 1) cornerRadius else 0.dp
+        entries.forEachIndexed { index, entry ->
+            val startRadius = if (index != 0) 0.dp else cornerRadius
+            val endRadius = if (index == entries.size - 1) cornerRadius else 0.dp
 
-                OutlinedButton(
-                    onClick = {
-                        selectedItem = index
-                        onSelectionChange.invoke(index)
-                    },
-                    modifier = Modifier
-                        .offset(if (index == 0) 0.dp else (-1 * index).dp, 0.dp)
-                        .zIndex(if (selectedItem == index) 1f else 0f),
-                    shape = RoundedCornerShape(
-                        topStart = startRadius,
-                        topEnd = endRadius,
-                        bottomStart = startRadius,
-                        bottomEnd = endRadius
-                    ),
-                    border = BorderStroke(
-                        1.dp,
-                        if (selectedItem == index) {
-                            MaterialTheme.colorScheme.primary
-                        } else {
-                            MaterialTheme.colorScheme.primary.copy(alpha = 0.75f)
-                        }
-                    ),
-                    colors = if (selectedItem == index) {
-                        ButtonDefaults.outlinedButtonColors(
-                            containerColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.2f),
-                            contentColor = MaterialTheme.colorScheme.primary
-                        )
+            OutlinedButton(
+                onClick = {
+                    selectedItem = index
+                    onSelectionChange.invoke(index)
+                },
+                modifier = Modifier
+                    .offset(if (index == 0) 0.dp else (-1 * index).dp, 0.dp)
+                    .zIndex(if (selectedItem == index) 1f else 0f),
+                shape = RoundedCornerShape(
+                    topStart = startRadius,
+                    topEnd = endRadius,
+                    bottomStart = startRadius,
+                    bottomEnd = endRadius
+                ),
+                border = BorderStroke(
+                    1.dp,
+                    if (selectedItem == index) {
+                        MaterialTheme.colorScheme.primary
                     } else {
-                        ButtonDefaults.outlinedButtonColors(
-                            containerColor = MaterialTheme.colorScheme.surface,
-                            contentColor = MaterialTheme.colorScheme.onSurface
-                        )
+                        MaterialTheme.colorScheme.primary.copy(alpha = 0.75f)
                     }
-                ) {
-                    Text(entry)
+                ),
+                colors = if (selectedItem == index) {
+                    ButtonDefaults.outlinedButtonColors(
+                        containerColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.2f),
+                        contentColor = MaterialTheme.colorScheme.primary
+                    )
+                } else {
+                    ButtonDefaults.outlinedButtonColors(
+                        containerColor = MaterialTheme.colorScheme.surface,
+                        contentColor = MaterialTheme.colorScheme.onSurface
+                    )
                 }
+            ) {
+                Text(entry)
             }
         }
     }
