39db4cf9f84ea6999e8e8b597ad12b7235834358
Bnyro <bnyro@tutanota.com>
2024-04-15 13:26:22 +0200

    chore: bump dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,18 +68,18 @@ android {
 dependencies {
     val compose_version: String by rootProject.extra
     // Core & Lifecycle
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("androidx.documentfile:documentfile:1.0.1")
-    implementation("androidx.work:work-runtime-ktx:2.8.1")
+    implementation("androidx.work:work-runtime-ktx:2.9.0")
 
     // Compose & UI
-    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.compose.ui:ui:$compose_version")
     implementation("androidx.compose.ui:ui-tooling-preview:$compose_version")
-    implementation("androidx.compose.material3:material3:1.2.0")
-    implementation("androidx.compose.material:material-icons-extended:1.4.3")
+    implementation("androidx.compose.material3:material3:1.2.1")
+    implementation("androidx.compose.material:material-icons-extended:1.6.5")
     implementation("com.github.nanihadesuka:LazyColumnScrollbar:1.9.0")
 
     // VCard
@@ -87,7 +87,7 @@ dependencies {
     implementation("net.lingala.zip4j:zip4j:2.11.5")
 
     // Image parsing
-    implementation("androidx.exifinterface:exifinterface:1.3.6")
+    implementation("androidx.exifinterface:exifinterface:1.3.7")
 
     // Phone number formatting
     implementation("com.googlecode.libphonenumber:libphonenumber:8.2.0")
@@ -97,9 +97,9 @@ dependencies {
     implementation("com.halilibo.compose-richtext:richtext-commonmark:0.17.0")
 
     // Room database
-    implementation("androidx.room:room-ktx:2.5.1")
-    implementation("androidx.navigation:navigation-compose:2.5.2")
-    kapt("androidx.room:room-compiler:2.5.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    kapt("androidx.room:room-compiler:2.6.1")
 
     // Testing
     testImplementation("junit:junit:4.13.2")
